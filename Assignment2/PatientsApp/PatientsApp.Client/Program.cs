994bfd5a589f9d948a9a11f0e2fa4b56d4f0af01
Davide Albiero <davide2910@live.it>
2023-03-17 16:07:03 +0100

    Create client app, implemented table and delete button

---
+++ Assignment2/PatientsApp/PatientsApp.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PatientsApp.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7265/")
+});
+
+await builder.Build().RunAsync();
--- Assignment2/PatientsApp/PatientsApp/Program.cs
+++ Assignment2/PatientsApp/PatientsApp/Program.cs
@@ -6,6 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder =>
+        builder.SetIsOriginAllowed(o => true)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
+
 
 var app = builder.Build();
 
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
