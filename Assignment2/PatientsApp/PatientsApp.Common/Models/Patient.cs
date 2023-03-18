f92201d89608fb4b04ca24ddc89533fc1f3d6820
Davide Albiero <davide2910@live.it>
2023-03-18 12:16:31 +0100

    Implemented new patient page

--- Assignment2/PatientsApp/PatientsApp.Common/Models/Patient.cs
+++ Assignment2/PatientsApp/PatientsApp.Common/Models/Patient.cs
@@ -1,24 +1,28 @@
-﻿namespace PatientsApp.Common.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientsApp.Common.Models;
 
 public class Patient
 {
     public Guid Id { get; set; }
 
+    [Required]
     public String FirstName { get; set; }
-
+    [Required]
     public String LastName { get; set; }
-
+    [Required]
     public String Email { get; set; }
-
+    [Required]
+    [MinLength(8)]
     public String Password { get; set; }
-
+    [Required]
     public String Gender { get; set; }
-
+    [Required]
     public DateOnly Birthdate { get; set; }
-
+    [Required]
     public String Address { get; set; }
-
+    [Required]
     public String Phone { get; set; }
-
+    [Required]
     public bool Terms { get; set; }
 }
